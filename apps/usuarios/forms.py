0b8baf0dec40f54490e06521f379765a29ae6650
Gaston Montero <gastonmontero29@gmail.com>
2025-12-05 15:25:04 -0300

    Verificador de email

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -9,3 +9,9 @@ class FormularioRegistroMiembro(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este email ya está registrado.")
+        return email
