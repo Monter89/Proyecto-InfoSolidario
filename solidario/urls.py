758dec8d72634a67036da00292d5fc9834b0c54b
David Luque <nft.deiv@gmail.com>
2025-12-12 21:01:47 -0300

    Configurar urls para servir media en Render

--- solidario/urls.py
+++ solidario/urls.py
@@ -1,9 +1,10 @@
 # solidario/urls.py (Proyecto Principal)
 
 from django.contrib import admin  # type: ignore
-from django.urls import path, include # type: ignore
+from django.urls import path, include, re_path 
 from django.conf import settings  # type: ignore
 from django.conf.urls.static import static  # type: ignore
+from django.views.static import serve
 from . import views 
 
 urlpatterns = [
@@ -11,16 +12,17 @@ urlpatterns = [
     path('', views.Home, name='path_home'),
     path('contacto/', views.Contacto, name='path_contacto'),
     
-    # 1. Rutas de Autenticación (Login, Logout, etc.)
     path('cuentas/', include('django.contrib.auth.urls')), 
-
-    # 2. Enlazar la nueva app 'articulos'
     path('articulos/', include('apps.articulos.urls')),
-    # 3. Enlazar la nueva app 'usuarios'
     path("usuarios/", include("apps.usuarios.urls")),
-
-    
 ] 
+
+# --- CONFIGURACIÓN PARA VER IMÁGENES EN RENDER ---
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
